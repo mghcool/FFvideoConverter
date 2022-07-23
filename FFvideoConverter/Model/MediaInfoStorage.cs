fc79e48a34ea36374026d12bd05944144f4ffb05
mghcool <mghcool@163.com>
2022-07-23 13:59:10 +0800

    增加model文件夹

--- FFvideoConverter/Model/MediaInfoStorage.cs
+++ FFvideoConverter/Model/MediaInfoStorage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FFvideoConverter
+﻿namespace FFvideoConverter.Model
 {
     /// <summary>
     /// 媒体信息储存
